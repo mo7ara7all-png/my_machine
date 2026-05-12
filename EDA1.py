8cf8908996ab30603657843e3ee139ec69652ba9
yaqeenbanisaker06-lgtm <yaqeenbanisaker06@gmail.com>
2026-05-12 22:32:13 +0300

    update EDA work

--- EDA1.py
+++ EDA1.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib
+
+import eda
 matplotlib.use("module://matplotlib_inline.backend_inline")
 import matplotlib.pyplot as plt
 import seaborn as sns
