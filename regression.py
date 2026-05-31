32a0222fddfb6552e9402ebed9503d80538efb5b
Moha <mo7ara7all-png>
2026-06-01 01:32:02 +0300

    Edit Regression

--- regression.py
+++ regression.py
@@ -1,4 +1,4 @@
-# %% -----------------Regression Task-------------------------------
+#  -----------------Regression Task-------------------------------
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,17 +20,12 @@ import scipy.stats as stats
 import statsmodels.api as sm
 from statsmodels.stats.diagnostic import het_breuschpagan
 
-
-
-# %% Load dataset
+# Load dataset
 df_clean = pd.read_csv("Data/OnlineNewsPopularity.csv")
 
 df_clean.columns = df_clean.columns.str.strip()
 
-
-
-
-# %% Prepare target variable
+# Prepare target variable
 df_clean["log_shares"]=np.log1p(df_clean["shares"])
 y=df_clean["log_shares"]
 
@@ -39,15 +34,10 @@ y=df_clean["log_shares"]
 # Log transformation helps make the target distribution more stable.
 #------------------------------------------------------------------------
 
-
-
-
-# %% We delete the columns that we do not want to use as inputs for the model
+# We delete the columns that we do not want to use as inputs for the model
 x=df_clean.drop(columns=["url","shares","log_shares"],errors='ignore')
 
-
-
-# %% We split the data into training and testing
+# We split the data into training and testing
 x_train,x_test,y_train,y_test=train_test_split (
     x,y,
     test_size=0.2,
@@ -55,25 +45,17 @@ x_train,x_test,y_train,y_test=train_test_split (
     
 )
 
-
-
-
-
-
 #----------------------------------------------
 # Feature scaling is applied because some models
 # are sensitive to feature magnitudes.
 #---------------------------------------------------
 
-# %% Feature scaling
+# Feature scaling
 scaler=StandardScaler()
 x_train_scaled=scaler.fit_transform(x_train)
 x_test_scaled=scaler.transform(x_test)
 
-
-
-
-# %% Function to evaluate regression models
+# Function to evaluate regression models
 def adjusted_r2_score(r2, n, p):
     return 1 - (1 - r2) * (n - 1) / (n - p - 1)
 
@@ -95,12 +77,7 @@ def evaluate_regression_model(model_name, y_test, y_pred, X_test):
         "R2": r2,
         "Adjusted R2": adj_r2
     }
-
-
-
-
-
-# %% Define regression models
+# Define regression models
 models = {
     "Linear Regression": LinearRegression(),
      "Ridge Regression": Ridge(alpha=1.0),
@@ -121,11 +98,7 @@ models = {
     random_state=42
 )
 }
-
-
-
-
-# %% Train, predict, and evaluate all regression models
+# Train, predict, and evaluate all regression models
 results = []
 predictions = {}
 
@@ -147,19 +120,13 @@ for model_name, model in models.items():
 
     results.append(result)
 
-
-
-
-# %% Let's convert the results into a table to compare the models
+# Let's convert the results into a table to compare the models
 results_df=pd.DataFrame(results)
 results_df=results_df.sort_values(by="RMSE")
 print(results_df)
 
 
-
-
-
-# %% Plotting a comparison between models based on RMSE
+# Plotting a comparison between models based on RMSE
 plt.figure(figsize=(12,6))
 
 sns.barplot(
@@ -175,25 +142,16 @@ plt.ylabel("RMSE")
 plt.show()
 plt.close()
 
-
-
-
-
-#%% We choose the best model according to the lowest RMSE
+# We choose the best model according to the lowest RMSE
 best_model_name = results_df.iloc[0]["Model"]
 
 best_y_pred = predictions[best_model_name]
 
 print("Best Regression Model:", best_model_name)
 
-
-
-
-
-# %% Compares the actual values with the expected values
+# Compares the actual values with the expected values
 plt.figure(figsize=(8,6))
 sns.scatterplot(x=y_test, y=best_y_pred)
-
 
 plt.xlabel("Actual Log Shares")
 plt.ylabel("Predicted Log Shares")
@@ -203,16 +161,11 @@ plt.show()
 plt.close()
 
 
-
-
-
-
-
 #----------------------------------------------------------
 # Residual analysis helps evaluate prediction errors.
 #----------------------------------------------------------
 
-#%% We calculate residuals, which are the difference between the actual value and the expected value
+# We calculate residuals, which are the difference between the actual value and the expected value
 residuals = y_test - best_y_pred
 
 plt.figure(figsize=(8,6))
@@ -228,9 +181,7 @@ plt.title(f"Residual Plot - {best_model_name}")
 plt.show()
 plt.close()
 
-
-
-#%% The chart shows the distribution of errors
+# The chart shows the distribution of errors
 plt.figure(figsize=(8,6))
 
 sns.histplot(residuals, kde=True, bins=40)
@@ -242,11 +193,7 @@ plt.show()
 plt.close()
 
 
-
-
-
-
-#%% This plot checks if the errors are close to a normal distribution
+# This plot checks if the errors are close to a normal distribution
 stats.probplot(residuals, dist="norm", plot=plt)
 
 plt.title(f"Q-Q Plot - {best_model_name}")
@@ -254,9 +201,7 @@ plt.title(f"Q-Q Plot - {best_model_name}")
 plt.show()
 plt.close()
 
-
-
-#%% The Breusch-Pagan test checks whether the variance of the errors is constant or not
+# The Breusch-Pagan test checks whether the variance of the errors is constant or not
 x_test_const = sm.add_constant(x_test_scaled)
 
 bp_test = het_breuschpagan(residuals, x_test_const)
@@ -273,11 +218,8 @@ bp_results = dict(zip(bp_labels, bp_test))
 print("Breusch-Pagan Test Results:")
 print(bp_results)
 
-
-
-#%% If the p-value is less than 0.05, this indicates the presence of heteroscedasticity
+# If the p-value is less than 0.05, this indicates the presence of heteroscedasticity
 if bp_results["p-value"] < 0.05:
     print("Heteroscedasticity detected.")
 else:
     print("No strong heteroscedasticity.")
-
